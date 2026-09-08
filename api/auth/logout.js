cd9675cd8f323da4843566c1ba796f2b8472405a
crowen420 <crowen421@gmail.com>
2026-09-08 17:16:25 -0400

    dad

--- api/auth/logout.js
+++
@@ -1,5 +0,0 @@
-const {SESSION_COOKIE,clearCookie,json}=require("../lib/kroger");
-module.exports=async function(req,res){
-  if(req.method!=="POST")return json(res,405,{error:"Method not allowed."});
-  return json(res,200,{ok:true},{"Set-Cookie":clearCookie(SESSION_COOKIE)});
-};
--- api/auth/status.js
+++
@@ -1,5 +0,0 @@
-const {userSession,json}=require("../lib/kroger");
-module.exports=async function(req,res){
-  try{if(req.method!=="GET")return json(res,405,{error:"Method not allowed."});return json(res,200,{connected:!!(await userSession(req,res))});}
-  catch(e){console.error(e);return json(res,500,{error:e.message||"Server error."})}
-};
